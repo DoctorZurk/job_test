6f44da20d2fdea8a271b1bcde341fe9321705524
Tommaso Lintrami <lintrami@yahoo.it>
2019-03-05 11:35:16 +0100

    First commit, half an hour

---
+++ main.js
@@ -0,0 +1,63 @@
+var curr_page = 1;
+
+$( function() {
+   
+
+    retrieve_users(curr_page);
+
+});
+
+// Delete runtime resource from Graph
+$("body").on("click", ".btn-delete", function(){
+	
+    if(confirm("are you sure you want to delete user: "+$(this).data("id") ) )
+    {
+        delete_user($(this).data("id"));
+    }
+});
+
+
+function retrieve_users(page){
+    $.ajax('https://reqres.in/api/users?page='+curr_page, {
+        data: {
+            per_page: 3
+        }
+    })
+    .then(
+        function success(obj) {
+            console.log(obj.data);
+            console.log(obj);
+
+            obj.data.forEach(function(item){
+                console.log(item['first_name']);
+                $('#mytable').append('<tr>');
+                $('#mytable').append ('<td><img src="'+item["avatar"]+'"></td><td>'+item["first_name"]+'</td><td>'+item["last_name"]+'</td><td><button class="btn btn-delete" data-id="'+item["id"]+'">DELETE</button></td>');
+
+                for(var i=0;i++;i<item.length)
+                $('#mytable').append('</tr>');
+
+            })
+
+            //alert('User\'s name is ' + obj.data.first_name);
+        },
+
+        function fail(data, status) {
+            alert('Request failed.  Returned status of ' + status);
+        }
+    );
+
+}
+
+function delete_user(theid){
+    $.ajax({
+        url: "https://reqres.in/api/users",
+        type: "DELETE",
+        data: {
+            id: theid
+        },
+        success: function(response){
+            console.log(response);
+        }
+    });
+                
+}
